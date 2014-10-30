158becc850c65a436a4fdee4e6b6deddc840de85
移动应用平台ExMobi <support@exmobi.cn>
2014-10-30 20:40:34 +0800

    解决iscroll内部点击一次变双击的bug
    解决iscroll内部点击一次变双击的bug

--- dist/agile/js/iscroll.js
+++ dist/agile/js/iscroll.js
@@ -1,5 +1,5 @@
 ﻿/*!
- * iScroll v4.2.5 ~ Copyright (c) 2012 Matteo Spinelli, http://cubiq.org
+ * iScroll v4.2.5 fixed ~ Copyright (c) 2012 Matteo Spinelli, http://cubiq.org
  * Released under MIT license, http://cubiq.org/license
  */
 (function(window, doc){
@@ -104,6 +104,9 @@ var m = Math,
 			topOffset: 0,
 			checkDOMChanges: false,		// Experimental
 			handleClick: true,
+			
+			preventGhostClick: true, // ex:prevent ghost clicks?防止2次点击
+			ghostClickTimeout: 500,   // ex:timeout for ghost click prevention设置时间差
 
 			// Scrollbar
 			hScrollbar: true,
@@ -490,6 +493,20 @@ iScroll.prototype = {
 		if (that.options.onScrollMove) that.options.onScrollMove.call(that, e);
 	},
 	
+	/**
+	* ex:Prevents any real clicks.
+	* See preventGhostClick portion of _end().
+	*/
+	_preventRealClick: function(e) {
+	    if (e._fake !== true) {
+	        e.preventDefault();
+	        e.stopPropagation();
+	        e.stopImmediatePropagation();
+	        e.cancel = true;
+	        return false;
+	    }
+	},
+	
 	_end: function (e) {
 		if (hasTouch && e.touches.length !== 0) return;
 
@@ -560,6 +577,17 @@ iScroll.prototype = {
 								e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
 								0, null);
 							ev._fake = true;
+							
+							//ex:
+							if (that.options.preventGhostClick) { //preventGhostClick: true,
+								// prevent ghost real clicks on body
+								document.body.addEventListener('click', that._preventRealClick, true);
+								// until ghost click timeout expires
+								setTimeout(function () {
+									document.body.removeEventListener('click', that._preventRealClick, true);
+								}, that.options.ghostClickTimeout);
+							}
+							
 							target.dispatchEvent(ev);
 						}
 					}, that.options.zoom ? 250 : 0);
